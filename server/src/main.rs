155e62d2234abc64dd4a95f4d4c69506ecfb7162
jbbe <jbbe@umich.edu>
2022-01-06 20:38:36 -0500

    Cors works, add svg to html

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,16 +1,16 @@
 #[macro_use] extern crate nickel;
 
-use nickel::Nickel;
-
+use nickel::{Nickel};
 fn main() {
     let mut server = Nickel::new();
 
+    server.utilize(nickel_cors::enable_cors);
     server.utilize(router! {
-        get "**" => |_req, _res| {
-            // _res.headers()
+        get "**" => |_req, mut res| {
+            res.headers_mut().set_raw("Access-Control-Allow-Origin", vec![b"*".to_vec()]);
+            res.headers_mut().set_raw("Access-Control-Allow-Headers", vec![b"Origin X-Requested-With Content-Type Accept".to_vec()]);
             shan_shui::svg_string(false)
         }
     });
-
     server.listen("127.0.0.1:6767").unwrap();
 }
