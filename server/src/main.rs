0bd5bbff8b6dcdf40c0fd56f2e06934df509bcde
jbbe <jbbe@umich.edu>
2022-01-06 21:58:54 -0500

    Background noise repeat

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,13 +3,18 @@
 use nickel::{Nickel};
 fn main() {
     let mut server = Nickel::new();
-
+    // let mut sessions = std::collections::HashMap::new();
+    // let shan_shui = 
     server.utilize(nickel_cors::enable_cors);
     server.utilize(router! {
         get "**" => |_req, mut res| {
             res.headers_mut().set_raw("Access-Control-Allow-Origin", vec![b"*".to_vec()]);
             res.headers_mut().set_raw("Access-Control-Allow-Headers", vec![b"Origin X-Requested-With Content-Type Accept".to_vec()]);
-            shan_shui::svg_string(false)
+            let mut painting = shan_shui::Painting::new();
+            // let id = rand::random::<i32>();
+            // sessions = sessions.insert(id, painting);
+            // shan_shui::svg_string(false)
+            painting.write_svg(256., 256.)
         }
     });
     server.listen("127.0.0.1:6767").unwrap();
