eaef1e294dbd318507f7815b9ca9a74b3ddcbdf7
jbbe <jbbe@umich.edu>
2022-01-08 16:04:26 -0500

    Moved native png utils into native crate

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1099,23 +1099,3 @@ impl Painting {
 }
 
 
-pub fn convert(in_file: &str, out_file: &str) {
-    println!("Writing final output to {:?}", out_file);
-    let mut opt = usvg::Options::default();
-    // Get file's absolute directory.
-    opt.resources_dir = std::fs::canonicalize(&in_file)
-        .ok()
-        .and_then(|p| p.parent().map(|p| p.to_path_buf()));
-    opt.fontdb.load_system_fonts();
-
-    let svg_data = std::fs::read(in_file).unwrap();
-    let rtree = usvg::Tree::from_data(&svg_data, &opt.to_ref()).unwrap();
-
-    let resolution = 512;
-
-    let fit_to = usvg::FitTo::Zoom(1.);
-    let mut pixmap = tiny_skia::Pixmap::new(resolution, resolution).unwrap();
-    resvg::render(&rtree, fit_to, pixmap.as_mut()).unwrap();
-
-    pixmap.save_png(out_file).unwrap();
-}
