9b990144518ae9f6a3d453ef0ec3894313a5c4c8
Josh Bell <joshua@th3rdcoast.com>
2023-03-28 18:45:39 -0400

    init paper lib

---
+++ paper/src/lib.rs
@@ -0,0 +1,16 @@
+
+extern "C" {
+    fn DEV_Digital_Write(input: i32) -> void;
+    fn DEV_Digital_Read(input: i32) -> i32;
+    
+    fn DEV_SPI_WriteByte(input: i32) -> i32;
+    fn DEV_SPI_Write_nByte(input: i32) -> i32;
+    fn DEV_Delay_ms(input: i32) -> i32;
+    
+    fn DEV_Module_Init() -> u8;
+    fn DEV_Module_Exit(input: i32) -> i32;
+}
+
+pub fn dev_module_init() {
+
+}
